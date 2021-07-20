0510674ac6b0744772929e5259d1010af868c64b
Morteza Gholami <mortezagh110@gmail.com>
2021-07-21 00:27:39 +0430

    splash and main screens created

---
+++ app/screens/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default Main = () => {
+  return (
+    <View>
+      <Text>This is home screen</Text>
+    </View>
+  )
+}
---
+++ app/screens/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default SplashScreen = () => {
+  return (
+    <View>
+      <Text>This is splash screen</Text>
+    </View>
+  )
+}
