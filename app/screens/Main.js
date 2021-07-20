afa1681b0e7b91a6aec2c990fee975e68df5cd65
Morteza Gholami <mortezagh110@gmail.com>
2021-07-21 01:09:57 +0430

    completed splash screen

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen'
+// import Icon from 'react-native-vector-icons/FontAwesome5'
+// import Theme from './app/scripts/theme'
 
 export default Main = () => {
   return (
--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,10 +1,49 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Image, StyleSheet} from 'react-native'
+import {StackActions} from '@react-navigation/native'
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen'
 
-export default SplashScreen = () => {
+export default SplashScreen = props => {
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     props.navigation.dispatch(StackActions.replace('Main'))
+  //   }, 3000)
+  // })
+
   return (
-    <View>
-      <Text>This is splash screen</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View
+        style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={{
+            width: wp(40),
+            height: wp(40),
+            marginTop: hp(20),
+            marginBottom: hp(2),
+          }}
+          resizeMode="center"
+        />
+        <Text style={styles.text}>CHAT</Text>
+        <Text style={styles.text}>PRACTICE</Text>
+      </View>
+      <Text
+        style={{paddingVertical: hp(2), fontSize: wp(4), textAlign: 'center'}}>
+        by Morteza Gholami
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: wp(8),
+  },
+})
